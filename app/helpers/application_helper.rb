5d1014e6ed42a0604fbaee793ca1ff7867f0a16f
root <root@alex-proger.(none)>
2012-03-20 17:18:31 +0400

    new +

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  def create_button(options={})
+    content_tag('div', submit_tag(t(:button_create),options) , :style=>"float:left")
+  end  
+  
+  def cancel_button(options={})
+    options = {:type => 'button', :onClick=>"parent.location='" + url_for(:action=>'index')+"'"} if !options.any?
+    content_tag('div', submit_tag(t(:button_cancel),options) , :style=>"float:left")
+  end  
+  
 end
