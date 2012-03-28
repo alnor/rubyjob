8b840a20f8458d712be77bc2c14eb9bfad0a3236
root <root@alex-proger.(none)>
2012-03-28 11:39:14 +0400

    new

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -11,6 +11,33 @@ class WorkersController < ApplicationController
     end     
   end
   
+  def create
+    @worker.attributes=params[:worker]
+    respond_to do |format|
+      if @worker.save
+        flash[:notice] = l(:notice_successful_create)
+        format.html { redirect_to :action => "edit", :id => @worker}
+        format.xml  { render      :xml    => @worker, :status => :created, :location => @worker }
+      else
+        format.html { render  :action=>'new'}
+        format.xml  { render  :xml    => @worker.errors}
+      end
+    end    
+  end
+  
+  def update
+    respond_to do |format|
+      if  @worker.update_attributes(params[:worker])
+        flash[:notice] = l(:notice_successful_update)
+        format.html { redirect_to :controller => 'worker', :action => "edit",:id => @vacancy }
+        format.xml  { head :ok }
+      else
+        format.html { render  :controller=>'worker', :action=>"edit", :id => @worker}
+        format.xml  { render :xml => @worker.errors }
+      end
+    end
+  end   
+  
   private
   
   def create_worker
