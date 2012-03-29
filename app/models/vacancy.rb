fd00e20545a669a8572c016fcbe26c70b1bba651
root <root@alex-proger.(none)>
2012-03-29 14:29:37 +0400

    lend

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -3,7 +3,7 @@ class Vacancy < ActiveRecord::Base
   has_many :skills, :through => :skills_vacancies
   
   validates :name, :presence => true 
-  validates :contacts, :format => { :with => /(^([a-z0-9_\-]+\.)*[a-z0-9_\-]+@([a-z0-9][a-z0-9\-]*[a-z0-9]\.)+[a-z]{2,4}$)|(\d+)/,
+  validates :contacts, :format => { :with => /(^([a-z0-9_\-]+\.)*[a-z0-9_\-]+@([a-z0-9][a-z0-9\-]*[a-z0-9]\.)+[a-z]{2,4}$)|(^[0-9]+$)/,
     :message => "Only phone or email allowed" }
 
   scope :active, where("lifetime > ?", Time.zone.now )
--- app/models/worker.rb
+++ app/models/worker.rb
@@ -3,7 +3,7 @@ class Worker < ActiveRecord::Base
   has_many :skills, :through => :skills_workers
   
   validates :name, :presence => true 
-  validates :contacts, :format => { :with => /(^([a-z0-9_\-]+\.)*[a-z0-9_\-]+@([a-z0-9][a-z0-9\-]*[a-z0-9]\.)+[a-z]{2,4}$)|(\d+)/,
+  validates :contacts, :format => { :with => /(^([a-z0-9_\-]+\.)*[a-z0-9_\-]+@([a-z0-9][a-z0-9\-]*[a-z0-9]\.)+[a-z]{2,4}$)|(^[0-9]+$)/,
     :message => "Only phone or email allowed" }  
     
   validates :name, :format => { :with => /^[а-яА-ЯёЁ ]+$/,
